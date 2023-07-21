8c3dea6c37996e62286034fc38aada4a0038a1cc
ruixuanzheng <96707192+ruixuanzheng@users.noreply.github.com>
2023-07-21 02:15:31 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get("/square/new", {:controller => "zebra", :action => "giraffe"})
+  get("/square/results", {:controller => "zebra", :action => "giraffe2"})
   
+  get("/square_root/new", {:controller => "zebra", :action => "elephant"})
+  get("/square_root/results", {:controller => "zebra", :action => "elephant2"})
+  
+  get("/payment/new", {:controller => "zebra", :action => "lion"})
+  get("/payment/results", {:controller => "zebra", :action => "lion2"})
+  
+  get("/", {:controller => "zebra", :action => "rabbit"})
+
 end
